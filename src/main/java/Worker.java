95ed98a30898609e72e6750a34ec39250f5df268
Yaroslav.Sinyatkin <yas_s@mail.ru>
2017-03-26 18:35:19 +0300

    list test

--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -1,7 +1,44 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * Created by yas on 26.03.2017.
+ * Класс с методами очистки Листа от повторяющихся элементов
  */
-public class Worker {
+public class Worker<T> {
+    final static int MAGIC_NUMBER = 3;
+  /**
+   * Удалить из List числа, повторяющиеся три и больше раз подряд
+   */
+  public void cleanList3AndMoreOneByOne(List<T> list) {
+    final HashMap<T, Integer> sameCount = new HashMap<>(); // хранит элемент листа и счетчик повторов
+    final Object[] lastNumber = {null}; // хранит предыдущее эначение
+    // заполняем мапу счетчиком элементов в list
+    list.forEach( (v)-> {
+      Integer counter = sameCount.containsKey(v) ? sameCount.get(v) : 0;
+      sameCount.put(v, v.equals(lastNumber[0]) || counter >= MAGIC_NUMBER ? counter + 1 : 1);
+      lastNumber[0] = v;
+    } );
+    // создаем список элементов для удаления
+    List<T> toRemove = list
+            .stream()
+            .filter( (v)-> sameCount.get(v) >= MAGIC_NUMBER)
+            .collect(Collectors.toList() );
+    list.removeAll(  toRemove );
+  }
+
+  /**
+   * Удалить из List числа, повторяющиеся три и больше раз не обязательно подряд
+   */
+  public void cleanList3AndMoreRandom(List<T> list) {
+    final HashMap<T, Integer> sameCount = new HashMap<>(); // хранит элемент листа и счетчик повторов
+    // заполняем мапу счетчиком элементов в list
+    list.forEach( (v)-> sameCount.put(v, sameCount.containsKey(v) ? sameCount.get(v) + 1 : 1 ) );
+    // создаем список элементов для удаления
+    List<T> toRemove = list
+            .stream()
+            .filter((v)-> sameCount.get(v) >= MAGIC_NUMBER)
+            .collect(Collectors.toList());
+    list.removeAll(  toRemove );
+  }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,46 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
- * Created by yas on 26.03.2017.
+ * Тест Main класса
  */
 public class MainTest {
+  @Test
+  public void cleanList3AndMoreOneByOne() throws Exception {
+    //prepare
+    List<Long> list = new ArrayList<>();
+    list.addAll(Arrays.asList(new Long[]{1l, 2l, 2l, 2l, 1l, 0l, 0l, 0l, 2l}));
 
+    //test
+    System.out.println("cleanList3AndMoreOneByOne got: "+ list);
+    new Worker<Long>().cleanList3AndMoreOneByOne(list);
+    System.out.println("cleanList3AndMoreOneByOne result: "+ list);
+
+    //check
+    assertTrue(list.size() == 2);
+    assertTrue(list.get(0) == 1);
+    assertTrue(list.get(1) == 1);
+  }
+
+  @Test
+  public void cleanList3AndMoreRandom() throws Exception {
+    //prepare
+    List<Integer> list = new ArrayList<>();
+    list.addAll(Arrays.asList(new Integer[]{2, 1, 2, 0, 2}));
+
+    //test
+    System.out.println("cleanList3AndMoreRandom got: " + list);
+    new Worker().cleanList3AndMoreRandom(list);
+    System.out.println("cleanList3AndMoreRandom result: " + list);
+
+    //check
+    assertTrue(list.size() == 2);
+    assertTrue(list.contains(0));
+    assertTrue(list.contains(1));
+  }
 }
